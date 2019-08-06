32111da463c2724cc1048c3eda6236a0aa6fd0c6
Eric Denovellis <edeno@bu.edu>
2019-08-06 10:47:07 -0400

    Remove symlink step because we can use glob instead.

--- franklab_mountainsort/__init__.py
+++ franklab_mountainsort/__init__.py
@@ -1,3 +1,2 @@
 # flake8: noqa
-from .core import (get_mda_files_dataframe, make_mda_ntrodeEpoch_links,
-                   move_mda_data, run_spike_sorting)
+from .core import get_mda_files_dataframe, move_mda_data, run_spike_sorting

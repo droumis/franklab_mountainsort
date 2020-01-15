14e2404862ca50012e429004767842f428704637
Eric Denovellis <edeno@bu.edu>
2020-01-14 16:38:25 -0800

    Only log the temp directory.

--- franklab_mountainsort/core.py
+++ franklab_mountainsort/core.py
@@ -60,12 +60,7 @@ def spike_sort_all(mda_file_info, input_path, output_path,
     electrodes = mda_file_info.groupby(
         ['animal', 'date', 'electrode_number'])
     logging.info(f'Processing {len(electrodes)} electrodes...')
-    logging.info(f'Input path: {input_path}')
-    logging.info(f'Output path: {output_path}')
-    temp_dir = os.getenv('ML_TEMPORARY_DIRECTORY')
-    logging.info(f'Temp directory: {temp_dir}')
-    log_dir = os.path.join(output_path, 'logs')
-    logging.info(f'Log Directory: {log_dir}')
+    logging.info(f'Temp directory: {os.getenv("ML_TEMPORARY_DIRECTORY")}')
 
     if len(electrodes) == 0:
         logging.warn('No electrodes for sorting found. Check to see if your '
